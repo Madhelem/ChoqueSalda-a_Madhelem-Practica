646e44f4f7ce03d0343e59677504967e016d9ecb
Madhelem <Madhelem.0509@gmail.com>
2021-04-15 00:00:37 -0400

    Ploblema7

--- demo/src/app.ts
+++ demo/src/app.ts
@@ -1,36 +1,25 @@
 
+/*Problema 7:
+Realice un programa en el cual se le proporcionara dos cadenas, y su trabajo consistirá en encontrar el número de caracteres similares que tiene entre ellos, incluyendo los repetidos.*/
+let str1 = 'zzzz'
+let str2 = 'zzzzzzz'
+let z = new Map()
 
-/*Los StrawhatPirates llegaron a un hotel el cual tiene la peculiaridad de que tiene habitaciones con 0 costo, la razón es que están embrujadas, dado que los invitados son bastante supersticiosos se niegan a ocupar una de esas habitaciones o CUALQUIERA DE LAS HABITACIONES que se encuentren por debajo de ellas.
-Ellos piden tu ayuda para determinar el costo total de las habitaciones que si pueden ocupar.
-Example*/
-
-let matrix:number[][] = [[0,0,0],
-                        [0,1,0],
-                        [1,1,1],
-                        
-    
-];
-let sum = 0;
-let max=matrix.length;
-for (var i = 0; i < matrix.length; i++)
-{
-   for (var y = 0; y <matrix[i].length; y++)
-   {
-            if(matrix[i][y] != 0){
-                  console.log (matrix[i][y]);
-                  if (i!=matrix.length-1)
-                  {
-                        if(matrix[i+1][y] != 0){
-                            sum = sum + matrix[i][y];
-                        }
-                  }
-                  else 
-                  {
-                         sum = sum + matrix[i][y];
-                  }
-            }
-
-
-   }  
+for(let i=0; i < str1.length;i++){
+   if(!z.get(str1[i])){
+      z.set(str1[i], 1)
+   }else{
+      z.set(str1[i], z.get(str1[i])+1)
+   }
+   //console.log(z.get(str1[i]))
 }
-console.log(sum);
+
+let cnt = 0;
+
+for(let i=0; i < str2.length;i++){
+   if(z.get(str2[i]) && z.get(str2[i]) > 0){
+      z.set(str2[i], z.get(str2[i])-1)
+      cnt = cnt +1;
+   }
+}
+console.log(cnt);
