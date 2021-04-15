bf63d9980ac0003b23d7306a858439214a3b6e5f
Madhelem <Madhelem.0509@gmail.com>
2021-04-14 23:56:03 -0400

    Ploblema2

--- demo/src/app.ts
+++ demo/src/app.ts
@@ -1,22 +1,12 @@
-/*Problema 1:
-Dado un numero n determinar si es primo o no*/
 
-let n:number = prompt("Introdusca un valor a calcular");
-//let j: number = 21;
-//console.log(primo(j));
-console.log(primo(n));
+/*Problema 2:
+Dada una cadena mostrarla invertida*/
 
-function primo(numero:number):boolean {
-  for (let i = 2; i < numero; i++) {
-    if (numero % i == 0) {
-      return false;
-    }
-
-  }
-  if(numero < 2){
-      return false;
-  }else{
-    return true;
-  }
-  
+let cadena:string = 'this is a sample string';
+let cadenarevertida:string = "";
+   
+for(let i = cadena.length-1; i>=0; i--)
+{
+  cadenarevertida += cadena[i];
 }
+console.log(cadenarevertida);
